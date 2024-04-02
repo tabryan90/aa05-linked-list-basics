0022a54a6a2a0febe974956439066fa2642a1785
Alex Bryan <tabryan1990@gmail.com>
2024-04-01 23:36:13 -0400

    phase 1 complete

--- linked-list.js
+++ linked-list.js
@@ -7,15 +7,39 @@ class LinkedListNode {
 
 class LinkedList {
   constructor() {
-    // Your code here 
+    this.head = null;
+    this.length = 0;
+    this.tail;
   }
 
   addToHead(val) {
-    // Your code here 
+    const newNode = new LinkedListNode(val, null);
+    this.length++;
+
+    newNode.next = this.head;
+
+    this.head = newNode;
+
+    if(!this.tail) this.tail = this.head;
+
   }
 
   addToTail(val) {
-    // Your code here 
+    const newNode = new LinkedListNode(val, null);
+    this.length++;
+
+    if(!this.head) {
+      return this.head = newNode;
+    }
+
+    let current = this.head;
+
+    while(current.next) {
+      current = current.next
+    }
+
+    current.next = newNode;
+
   }
 
   // You can use this function to help debug
